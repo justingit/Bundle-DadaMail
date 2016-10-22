ef29b289eea30055238d8fd2d19afb3549d21dd7
justingit <perl@justinsimoni.com>
2016-10-22 13:05:13 -0600

    It's shaping up to look like a real module, now!

--- lib/Bundle/DadaMail.pm
+++ lib/Bundle/DadaMail.pm
@@ -1,54 +1,91 @@
 package Bundle::DadaMail;
 
-
 $VERSION = '0.0.1';
 
 1;
 
 __END__
 
+=head1 NAME 
+
+C<Bundle::DadaMail> - CPAN Bundle for optional CPAN modules used in Dada Mail
+
+=head1 SYNOPSIS
+
+	perl -MCPAN -e 'install Bundle::DadaMail'
+
+or similar CPAN module installer method
+
+=head1 Description
+
+C<Bundle::DadaMail> is a CPAN Bundle of I<optional> CPAN modules used by Dada Mail. 
+Dada Mail is a self-hosted mailing list manager and the distribution does include the
+CPAN modules that it requires.
+
+The modules listed here are not included for a variety of reasons, 
+but mostly because their own dependency chain is very long, or that they
+or some dependency that they require needs compilation, or even an outside library. 
+
+=head1 See Also
+
+L<http://dadamailproject.com>
+
 =head1 CONTENTS
 
-LWP
+LWP - So many things. Makes the Send a Webpage work, for starters.  actually a required module, but we assume the environment Dada Mail is installed on has this already (I know, never assume!)
 
-CSS::Inliner
+IO::Socket::SSL
 
-HTML::Packer
+CSS::Inliner - Used in inlining CSS in HTML email messages. Important for making sure HTML messages look correctly in most all readers.
 
-CSS::Packer
+HTML::Packer - minifies HTML used in HTML email messages
 
-HTML::Scrubber
+CSS::Packer - minifies CSS used in HTML email messages
 
-DateTime::Event::Recurrence
+HTML::Scrubber - removes Javascript in messages - think discussion lists
 
-HTML::Tree
+DateTime::Event::Recurrence - used for scheduled mass mailings
 
-HTML::Element
+DateTime - same
 
-HTML::TreeBuilder
+HTML::Tree - used for Dada Mail's "Magic" templates, as well as manipulating HTML documents
 
-DateTime
+HTML::Element - same
 
-DateTime::Event::Recurrence
-		
+HTML::TreeBuilder - same
+
 Google::reCAPTCHA
 
 Gravatar::URL
 
 Net::Domain
 
-JSON
+JSON - actually required for Dada Mail - Pure Perl version included, but you probably want to use a faster version
 
 MIME::Base64
 
 Net::DNS;
 
-Text::CSV
+Text::CSV - actually required for Dada Mail - Pure Perl version included, but you probably want to use a faster version
 
-URI::GoogleChart
+URI::GoogleChart - used for the fancy charts Dada Mail's Tracker plugin uses. 
 
-HTML::FormatText::WithLinks
+HTML::FormatText::WithLinks - Plaintext to HTML
 
 Captcha::reCAPTCHA::Mailhide
 
-DBI
+DBI - actually a required module, but we assume the environment Dada Mail is installed on has this already (I know, never assume!)
+
+XMLRPC::Lite
+
+Cwd - for Amazon SES
+
+Digest::SHA - for Amazon SES
+
+URI::Escape - for Amazon SES
+
+MIME::Base64 - for Amazon SES
+
+Crypt::SSLeay - for Amazon SES
+
+XML::LibXML - for Amazon SES
