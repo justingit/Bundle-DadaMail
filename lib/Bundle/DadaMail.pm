bce98295bd9a664d1c15a684ec91e590cc0ca62f
justingit <perl@justinsimoni.com>
2022-08-16 09:51:08 -0600

    Bumping this up

--- lib/Bundle/DadaMail.pm
+++ lib/Bundle/DadaMail.pm
@@ -1,6 +1,6 @@
 package Bundle::DadaMail;
 
-$VERSION = '0.0.15';
+$VERSION = '0.0.16';
 
 1;
 
@@ -20,20 +20,28 @@ or similar CPAN module installer method
 
 C<Bundle::DadaMail> is a CPAN Bundle of all CPAN modules required to run Dada Mail. 
 
-Some modules are shipped with the app itself. These modules are for the most part Pure Perl. You may not want to use the included (unmanaged, and thus easy to become outdated) distribution, so we suggest installing C<Bundle::DadaMail> instead and letting your usually perl sys admin tools handle things in whatever way you do that.  
+Some modules are shipped with the app itself. These modules are for the most part Pure Perl. You may not want to use the included perllib, so we suggest installing C<Bundle::DadaMail> instead and letting your usual perl sys admin tools handle things in whatever way you do that. 
 
-Those modules are listed in, C<Bundle::DadaMail::IncludedInDistro>. Installing this module will also install everything in, C<Bundle::DadaMail::IncludedInDistro>.
+Those modules are listed in, C<Bundle::DadaMail::IncludedInDistro>. Installing this module will also install that bundle.
 
-There are optional modules that Dada Mail can utilize to extend its functionality. They are listed in, C<Bundle::DadaMailXXL> and installing C<Bundle::DadaMailXXL> will isntall everything in C<Bundle::DadaMailXXL> and C<Bundle::DadaMail::IncludedInDistro>. 
+There are also optional modules that Dada Mail can utilize to extend its functionality. They are listed in, C<Bundle::DadaMailXXL> and installing C<Bundle::DadaMailXXL> will install the two bundles already mentioned.
+
+=head2 Backend Details
+
+Dada Mail supports running using SQLite, MySQL or PostgreSQL. This bundle only asks to install C<DBD::SQLite>. If you would like to run Dada Mail under a different backend, you will need to install those drivers separately. 
 
 =head1 See Also
 
-L<http://dadamailproject.com>
+L<https://dadamailproject.com>
 
 L<https://github.com/justingit/Bundle-DadaMail>
 
 =head1 CONTENTS
 
 Bundle::DadaMail::IncludedInDistro
+
+DBI
+
+DBD::SQLite
 
 JSON - actually required for Dada Mail - Pure Perl version included, but you probably want to use a faster version
